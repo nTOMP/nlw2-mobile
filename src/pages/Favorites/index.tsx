c44dd29d6a819c459e0af39d8351a418a5fbc713
Felipe Dias <tomp666@gmail.com>
2020-08-10 14:55:43 -0300

    Aula 05. Versão inicial.

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useFocusEffect } from '@react-navigation/native'
 
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import AsyncStorage from '@react-native-community/async-storage';
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if(response) {
+        setFavorites(JSON.parse(response));
+      }
+    })
+  }
+
+  useFocusEffect(React.useCallback(loadFavorites, []))
+
   return (
     <View style={styles.container}>
       <PageHeader title="Meus Proffys favoritos" />
+      <ScrollView 
+        style={styles.teacherList}
+        contentContainerStyle= {{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {favorites.map((teacher: Teacher) =>
+          <TeacherItem 
+            key={teacher.id}
+            teacher={teacher}
+            favorited
+          />
+        )}
+      </ScrollView>
     </View>
   );
 }
